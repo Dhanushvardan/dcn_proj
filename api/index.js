d45bd0a444bf8536278f1b8bf14a5a3ac495cbb5
Dhanushvardan <avj25012003@gmail.com>
2022-12-25 12:02:26 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const { Server } = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("server_id:" + socket.id);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+    });
+    socket.on("sending_msg", (data) => {
+
+        socket.to(data.room).emit("return_msg", data.msg);
+    });
+
+    socket.on("gridValueEmit", (data) => {
+
+        socket.to(data.room).emit("gridValueReturn", data.mat);
+    })
+
+    socket.on("gridValueEmit1", (data) => {
+
+        socket.to(data.room).emit("gridValueReturn1", data.mat1);
+    })
+    socket.on("gridValueEmit2", (data) => {
+
+        socket.to(data.room).emit("gridValueReturn2", data.mat2);
+    })
+
+
+})
+
+server.listen(3001, () => {
+    console.log("server started at 3001");
+})
